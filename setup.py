3ad0a01893309325aaef25629f81062e17787350
Mohamed Fazil <mfazil@hello-robot.com>
2024-04-08 20:37:19 -0700

    make it a package

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyrtc',
+    version='0.1',
+    license='MIT',
+    description='An example Python package',
+    url="https://github.com/hello-fazil/pyRTC",
+    long_description=open('README.md').read(),
+    install_requires=[
+        'aiortc'
+    ],
+    packages=find_packages(include=['pyrtc', 'pyrtc.video_transceiver', 'pyrtc.helpers']), 
+    author='Mohamed Fazil',
+    author_email='mohamedfazilsulaiman@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
